bce5484fc276a81a9659037f8856b3f8d24cad33
tropjcal <84661170+tropjcal@users.noreply.github.com>
2023-01-19 16:19:50 +1100

    Remove hard coded numbers for TSN location
    Subbed for constants instead. Much can be done to improve the coding (ask for/detect program file path, improved efficiency, but too lazy and don't have access to the example file anymore. Oh well!

--- adobe-autorenew.py
+++ adobe-autorenew.py
@@ -3,23 +3,24 @@
 
 import re
 
+TSN_LINE_NO = 5 # TSN (Trial Serial Number) located on line 5
+TSN_WORD_NO = 23 # After parsing, TSN is word 23 of line 5
+TSN_COL_NO = 232 # TSN begins from column 23
+
 f = open("C:\Program Files\Adobe\Adobe InDesign 2021\AMT\\application.xml", "r")
 f_lines = f.readlines()
 f.close()
 
-# Line 5 is where trial serial number is located
-line = f_lines[5].strip("\n|\t")
-line_list = re.split(' |="|">|</', line)
+line = f_lines[TSN_LINE_NO].strip("\n|\t")
+line_list = re.split(' |="|">|</', line) # Parsing and removing unnecessary characters
 
-# Index of tsn is 23
-current_tsn = line_list[23]
-new_tsn = (int(line_list[23]) + 1)
+current_tsn = line_list[TSN_WORD_NO]
+new_tsn = (int(line_list[TSN_WORD_NO]) + 1)
 
-f_lines[5] = f_lines[5][:232] # Cut 916587039040963386426082</Data></Other> from end of line (example tsn)
-f_lines[5] += (str(new_tsn) + "</Data></Other>\n") # Replace with new tsn
+f_lines[TSN_LINE_NO] = f_lines[TSN_LINE_NO][:TSN_COL_NO] # Cut 916587039040963386426082</Data></Other> from end of line (example tsn)
+f_lines[TSN_LINE_NO] += (str(new_tsn) + "</Data></Other>\n") # Replace with new tsn
 
 fw = open("C:\Program Files\Adobe\Adobe InDesign 2021\AMT\\application.xml", "w")
 for l in f_lines:
-    fw.write(l)
-    
+    fw.write(l)    
 fw.close()
